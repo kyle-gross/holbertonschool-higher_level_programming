65ed3dcfad6ddde766c9fcdb08629bbf375a1daf
kyle-gross <kyg555@outlook.com>
2021-04-27 22:05:22 +0000

    remove helper func, mod existing code to accomodate

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,18 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-listint_t *add_node_front(listint_t **head, const int n)
-{
-	listint_t *temp;
-
-	temp = malloc(sizeof(listint_t));
-	if (!temp)
-		return (NULL);
-	temp->n = n;
-	temp->next = *head;
-	*head = temp;
-	return (temp);
-}
 /**
  * insert_node - inserts a node somewhere in the middle of a linked list
  * @head: pointer to head of list
@@ -36,7 +24,8 @@ listint_t *insert_node(listint_t **head, int number)
 	trav = *head;
 	if (number <= trav->n)
 	{
-		new_node = add_node_front(head, number);
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
 	while (number >= trav->next->n && trav->next)
