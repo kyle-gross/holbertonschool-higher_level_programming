647d90cb6d5f9972a071b50aeb02147a4bfbfcac
kyle-gross <kyg555@outlook.com>
2021-04-27 23:35:52 +0000

    Added id logic

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in str:
-        if i == (n - 1):
-            continue
-        print("{}".format(i), end="")
-    print()
+    if len(str) > n and n >= 0:
+        str = str[0: n: ] + str[n + 1: : ]
+    return str
