0076ebc2db24009f9349f7cdb3d2795745f422d2
kyle-gross <kyg555@outlook.com>
2021-04-27 19:19:22 +0000

    Add task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+n = 0
+for i in range(0, 10):
+    ii = 1 + n
+    while ii < 10:
+        print("{}{}".format(i, ii), end="")
+        if i == 8 and ii == 9:
+            break
+        ii += 1
+        print(",", end=" ")
+    n += 1
+print("")
