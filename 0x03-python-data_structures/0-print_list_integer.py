66ec23140abbad95bba7de1548efda220abd63e4
kyle-gross <kyg555@outlook.com>
2021-04-28 17:22:17 +0000

    Add task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Function to print all integers of a list
+def print_list_integer(my_list=[]):
+    for i in range(my_list)
+        # One integer per line
+        # Use str.format() to print integers
+        print("{:d}".format(i))
