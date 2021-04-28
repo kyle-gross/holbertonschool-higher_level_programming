23b78ca367f1d2c80025276ee5593c40de09e298
kyle-gross <kyg555@outlook.com>
2021-04-28 17:38:57 +0000

    Fix syntax for for loop and index()

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 # Function to print all integers of a list
 def print_list_integer(my_list=[]):
-    for i in range(my_list)
-        # One integer per line
-        # Use str.format() to print integers
-        print("{:d}".format(i))
+    for i in my_list:
+        print("{:d}".format(my_list.index(i))) # One integer per line, str.format()
