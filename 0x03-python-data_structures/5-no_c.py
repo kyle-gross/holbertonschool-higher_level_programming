e4d6af3406172ce57d19a3a803d30db0691038a2
kyle-gross <kyg555@outlook.com>
2021-04-28 19:06:14 +0000

    Fix pep8

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     # Removes all characters c and C from a string
     if my_string:
-    # Return the new string
+        # Return the new string
         return my_string.translate({ord(i): None for i in 'Cc'})
