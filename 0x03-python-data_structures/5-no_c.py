c00244ef9531444a2cbd6c8071031f01b21dd25d
kyle-gross <kyg555@outlook.com>
2021-04-28 19:05:41 +0000

    Add task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    # Removes all characters c and C from a string
+    if my_string:
+    # Return the new string
+        return my_string.translate({ord(i): None for i in 'Cc'})
