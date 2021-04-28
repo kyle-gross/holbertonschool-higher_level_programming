6ed1b7d94ed4eb4b63f361cd3e94788ce9893d3a
kyle-gross <kyg555@outlook.com>
2021-04-28 18:37:06 +0000

    Add null/none check

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     # Prints all integers of a list, in reverse order
-    my_list.reverse()
-    for i in my_list:
-        # One integer per line
-        # Use str.format() to print integers
-        print("{:d}".format(i))
+    if my_list:
+        my_list.reverse()
+        for i in my_list:
+            # One integer per line
+            # Use str.format() to print integers
+            print("{:d}".format(i))
