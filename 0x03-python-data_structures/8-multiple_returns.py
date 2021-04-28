a7cfc9a7fa21a242598465a016a989344492db0a
kyle-gross <kyg555@outlook.com>
2021-04-28 21:14:31 +0000

    Add check for empty sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    firstCharacter = sentence[:1]
+    if not sentence:
+        firstCharacter = None
+    else:
+        firstCharacter = sentence[:1]
     # Return tuple with the length of string and first character
     return (length, firstCharacter)
