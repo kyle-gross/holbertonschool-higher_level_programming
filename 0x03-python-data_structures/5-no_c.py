a82b016c92b25fb15067f84128d9894306f164a4
kyle-gross <kyg555@outlook.com>
2021-04-28 19:13:59 +0000

    Remove null check

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     # Removes all characters c and C from a string
-    if my_string:
-        # Return the new string
-        return my_string.translate({ord(i): None for i in 'Cc'})
+    # Return the new string
+    return my_string.translate({ord(i): None for i in 'Cc'})
