ac519bf3e028de8c96f6628e550987d922725c2f
kyle-gross <kyg555@outlook.com>
2021-04-28 17:44:43 +0000

    changed syntax in print

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@ def print_list_integer(my_list=[]):
     # Function to print all integers of a list
     for i in my_list:
         # One integer per line, str.format()
-        print("{:d}".format(my_list.index(i)))
+        print("{:d}".format(i))
