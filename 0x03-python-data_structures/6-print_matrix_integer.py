9f39671d92307a3d6744f8942d03b9443c34207d
kyle-gross <kyg555@outlook.com>
2021-04-28 19:56:37 +0000

    Fix extra space at end of lines

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     # Prints a matrix of integers
     for i in range(len(matrix)):
         for ii in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][ii]), end=" ")
+            print("{:d}".format(matrix[i][ii]), end="")
+            if ii < (len(matrix[i]) - 1):
+                print(" ", end = "")
         print()
