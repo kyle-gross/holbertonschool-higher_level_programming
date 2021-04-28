c01fb5e9af63b7ae52f5a8eb465658b6f22ceeaa
kyle-gross <kyg555@outlook.com>
2021-04-28 01:47:37 +0000

    Fix PEPY

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     dirlen = len(dir(hidden_4))
     for i in range(dirlen):
         names = dir(hidden_4)[i]
-        if names.startswith("__") == True:
+        if names.startswith("__"):
             continue
         print("{}".format(names))
