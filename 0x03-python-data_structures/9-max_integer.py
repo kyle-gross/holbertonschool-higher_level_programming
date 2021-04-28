483dc458370e3a4f667155b0ff4c6259e5dd2337
kyle-gross <kyg555@outlook.com>
2021-04-28 21:24:02 +0000

    using sort instead of a loop :D

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,5 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    highest = 0
-    for i in my_list:
-        if i > highest:
-            highest = i
-    return highest
+    my_list.sort()
+    return my_list[-1]
