9e409d19a0a8f146ab267230337cd826a87c1113
kyle-gross <kyg555@outlook.com>
2021-05-04 17:45:23 +0000

    add doc and fix pep8

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""This module creates a new class called Square.
+The size of the square is initialized"""
+
+
 class Square:
+    """Square class initializes the size ass a private instance attribute"""
     def __init__(self, size):
+        """init function which initializes a Square object
+        and gives it a size"""
         self.__size = size
