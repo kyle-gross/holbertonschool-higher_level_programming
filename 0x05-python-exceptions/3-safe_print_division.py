b833c2f0cab6f8682bd0e1cd64a4ca70b6f1ed4a
kyle-gross <kyg555@outlook.com>
2021-05-04 17:32:29 +0000

    Change placement of "inside result" string

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,11 +6,10 @@ safe_print_division - prints the result of division of a and b
 
 def safe_print_division(a, b):
     """ prints and returns within finally"""
-    print("Inside result:", end="")
     try:
         quotient = a / b
     except ZeroDivisionError:
         quotient = None
     finally:
-        print("{}".format(quotient))
+        print("Inside result: {}".format(quotient))
         return quotient
