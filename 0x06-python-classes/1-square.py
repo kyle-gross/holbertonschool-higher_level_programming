70cae76461df6ca67148ce69a1a610b21c040f37
kyle-gross <kyg555@outlook.com>
2021-05-04 17:54:47 +0000

    Add doc, fix typo

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,7 +4,7 @@ The size of the square is initialized"""
 
 
 class Square:
-    """Square class initializes the size ass a private instance attribute"""
+    """Square class initializes the size as a private instance attribute"""
     def __init__(self, size):
         """init function which initializes a Square object
         and gives it a size"""
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-class Square:
-    __size = 0
+"""This module creates a new class called Square.
+The size of the square is initialized"""
 
+
+class Square:
+    """Square class initializes the size as a private instance attribute"""
     def __init__(self, size):
+        """init function which initializes a Square object
+        and gives it a size.
+        Raises a TypeError if given size is not an integer.
+        Raises a ValueError if size is less than 0."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
