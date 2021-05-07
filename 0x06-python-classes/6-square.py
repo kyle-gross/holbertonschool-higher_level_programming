9e8ed6cdf0bfed858820ec890f38d6eeb8b2bf05
kyle-gross <kyg555@outlook.com>
2021-05-07 23:47:34 +0000

    Changed isinstance to type check

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,7 +42,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if min(value) < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
+        if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
