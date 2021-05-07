75c64af56d0f9fe749acf2819690c3f949dd5840
kyle-gross <kyg555@outlook.com>
2021-05-07 23:46:12 +0000

    Attempt at fixing bad type error in position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,12 +40,11 @@ class Square:
         containing 2 integers or a value is < 0"""
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif min(value) < 0:
+        if min(value) < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif not isinstance(value[0], int) or not isinstance(value[1], int):
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
         """This function returns the area of a square object"""
