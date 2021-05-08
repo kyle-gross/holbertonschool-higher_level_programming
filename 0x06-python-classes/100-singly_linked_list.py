97811134bb36d5443217a6d114c571ad10dc5e08
kyle-gross <kyg555@outlook.com>
2021-05-08 20:41:59 +0000

    Add task 7

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""This module contains the class Node which defines
+   a node of a singly linked list
+"""
+
+
+class Node:
+    """This class creates nodes for a singly linked list"""
+    
+    def __init__(self, data, next_node=None):
+        """This method constructs a node"""
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        """Getter for data"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Setter for data"""
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        """Getter for next_node"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Setter for next_node"""
+        if not isinstance(value, Node) and value != None:
+            raise TypeError("next node must be a Node object")
+        else:
+            self.__next_node = value
+
+class SinglyLinkedList:
+    """This class creates singly linked lists"""
+    def __init__(self):
+        """Constructs a linked list object"""
+        self.head = None
+
+    def sorted_insert(self, value):
+        """Inserts a node in a linked list ordered by value"""
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return self.head
+
+        temp = self.head
+        if value <= temp.data:
+            new_node.next_node = temp
+            self.head = new_node
+            return
+
+        while temp.next_node:
+            if temp.next_node.data > value:
+                break
+            temp = temp.next_node
+
+        new_node.next_node = temp.next_node
+        temp.next_node = new_node
+        return self.head
+
+    def __str__(self):
+        data = []
+        current = self.head
+        while current is not None:
+            data.append(current.data)
+            current = current.next_node
+        return "%s" %('\n'.join(str(i) for i in data))
