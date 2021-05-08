9f3395b08935106daf6eeb905d2169c0a82da9d6
kyle-gross <kyg555@outlook.com>
2021-05-08 05:10:55 +0000

    Add task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This method contains a function that prints an integer
+"""
+
+
+def safe_print_integer_err(value):
+    """This function safely prints integers"""
+    try:
+        print("{:d}".format(value))
+        return True
+
+    except ValueError:
+        return False
