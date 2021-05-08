c345e94ccb38188cdaa647e26f225f7edabcf3af
kyle-gross <kyg555@outlook.com>
2021-05-08 05:23:27 +0000

    Now printing out error msg to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,11 @@
 
 def safe_print_integer_err(value):
     """This function safely prints integers"""
+    import sys
     try:
         print("{:d}".format(value))
         return True
 
-    except (ValueError, TypeError):
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
