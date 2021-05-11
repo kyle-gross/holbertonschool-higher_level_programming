ad8a3e841da0b8fac2a2fa245a9e2a3de861f9d1
kyle-gross <kyg555@outlook.com>
2021-05-11 22:21:49 +0000

    Add task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contains the Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class inherits from the Rectangle class"""
+    def __init__(self, size):
+        """This function constructs a square object"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """This magic method returns a string to represent a square"""
+        return "[Square] " + str(size) + "/" + str(size)
+
+    def area(self):
+        """This function calculates the area of a square"""
+        return self.__size ** 2
