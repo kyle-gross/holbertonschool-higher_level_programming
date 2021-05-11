26da42ffcbcb7cee46401bd2c8fbc0e86d3c025d
kyle-gross <kyg555@outlook.com>
2021-05-11 21:33:36 +0000

    Re format validation

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -10,14 +10,14 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """This constructs Rectangle objects"""
         super().__init__()
-        if BaseGeometry.integer_validator(self, "width", width):
-            self.__width = width
-        if BaseGeometry.integer_validator(self, "height", height):
-            self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def __str__(self):
         """This is a magic method which returns a rectangle string"""
-        return "[Rectangle]" + str(self.__width) + "/" + str(self.__height)
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
 
     def area(self):
         """This method returns the area of a Rectangle object"""
