a28f4f28f8ddd7df95bfbf58bd48a08124442edd
kyle-gross <kyg555@outlook.com>
2021-05-11 19:28:26 +0000

    Add type check

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,7 +8,7 @@ def is_same_class(obj, a_class):
     """This function returns True if the object is exactly an instance
        of the specified class; otherwise False
     """
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) == a_class:
         return True
     else:
         return False
