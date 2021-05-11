3878beef6ca16fc9af5e7a6d181f64f03cee2905
kyle-gross <kyg555@outlook.com>
2021-05-11 21:16:42 +0000

    Add task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module contains the class BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This class inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """This constructs Rectangle objects"""
+        super().__init__()
+        if BaseGeometry.integer_validator(self, "width", width):
+            self.__width = width
+        if BaseGeometry.integer_validator(self, "height", height):
+            self.__height = height
+
+    def __str__(self):
+        """This is a magic method which returns a rectangle string"""
+        return "[Rectangle]" + str(self.width) + "/" + str(self.height)
+
+    def area(self):
+        """This method returns the area of a Rectangle object"""
+        return self.width * self.height
