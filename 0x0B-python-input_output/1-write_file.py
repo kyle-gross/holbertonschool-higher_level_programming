b8e670ba9c4ac3402218ccd4bac038154b725993
kyle-gross <kyg555@outlook.com>
2021-05-12 17:44:51 +0000

    Add task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module contains a function that writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """This function writes <text> to <filename>"""
+    with open(filename, mode="w", encoding="utf-8") as afile:
+        afile.write(text)
