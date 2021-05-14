1a25a4baf740a4bf4b55045a7c74c5a98789fb18
kyle-gross <kyg555@outlook.com>
2021-05-14 22:25:15 +0000

    Update index of i

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -38,7 +38,7 @@ class Square(Rectangle):
         for i in inspect.getmembers(self):
             if not i[0].startswith("_"):
                 if not inspect.ismethod(i[1]):
-                    if i[1] == "height" or i[1] == "width":
+                    if i[0] == "height" or i[0] == "width":
                         continue
                     my_dict[i[0]] = i[1]
         return my_dict
