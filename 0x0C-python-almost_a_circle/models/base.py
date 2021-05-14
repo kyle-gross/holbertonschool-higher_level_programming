908bf6c0a5767380c17e65feb79352b092c08fe6
kyle-gross <kyg555@outlook.com>
2021-05-14 17:59:09 +0000

    Add models dir

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains the Base class"""
+
+
+class Base:
+    """This class is the base of all other classes in this project"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """This is the base level constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
