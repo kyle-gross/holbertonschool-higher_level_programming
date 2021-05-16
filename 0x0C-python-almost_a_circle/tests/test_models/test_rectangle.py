18919a9d12ba2ea01068fcf127817050a9941099
kyle-gross <kyg555@outlook.com>
2021-05-16 22:36:14 +0000

    Add rectangle tests

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module contains unit tests for Rectangle"""
+
+
+import unittest
+from models.base import Base
+from models.recangle import Rectangle
+
+
+class TestRectangle(unittest.Testcase):
+    """This class tests the Rectangle class"""
+
+    def test_rectangle_exists(self):
+        rect = Rectangle(1, 2, 1, 2)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 1)
+        self.assertEqual(rect.y, 2)
