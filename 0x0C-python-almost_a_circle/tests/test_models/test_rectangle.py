ee1f252d741007bfde79c3c4841a556f35d6c659
kyle-gross <kyg555@outlook.com>
2021-05-16 22:49:27 +0000

    Add error checks

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,12 +10,24 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     """This class tests the Rectangle class"""
 
-    def test_rectangle_exists(self):
+    def test_rectangle_2_args(self):
         rect = Rectangle(1, 2)
         self.assertEqual(rect.width, 1)
         self.assertEqual(rect.height, 2)
+
+    def test_rectangle_4_args(self):
         rect = Rectangle(1, 2, 1, 2)
         self.assertEqual(rect.width, 1)
         self.assertEqual(rect.height, 2)
         self.assertEqual(rect.x, 1)
         self.assertEqual(rect.y, 2)
+
+    def test_rectangle_type_error(self):
+        with assertRaises(TypeError):
+            rect = Rectangle("1", 2)
+        with assertRaises(TypeError):
+            rect = Rectangle(1, "2")
+
+    def test_rectangle_val_error(self):
+        with assertRaises(ValueError):
+            rect = Rectangle(0, 1)
