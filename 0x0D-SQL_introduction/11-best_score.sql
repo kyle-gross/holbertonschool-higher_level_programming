dd734196f8d9e3fa6338d6a89c1288348f165a64
kyle-gross <kyg555@outlook.com>
2021-05-26 21:15:28 +0000

    Use WHERE

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,4 @@
 -- lists all records of the table second_table with a score >= 10
-SELECT IF score >= 10, name FROM second_table ORDER BY score DESC, name;
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC, name;
