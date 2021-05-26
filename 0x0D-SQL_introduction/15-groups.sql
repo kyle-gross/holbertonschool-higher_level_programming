3608af6e3bcdbb2b470c6ae06ab293b9b66450f1
kyle-gross <kyg555@outlook.com>
2021-05-26 21:44:45 +0000

    Change SELECT COUNT(score) to SELECT score, COUNT(score)

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score in the table second_table
-SELECT COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
