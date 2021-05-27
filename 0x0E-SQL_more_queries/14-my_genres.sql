2eeceb95ae89d3bdac2797513c6aeec5a8ce2119
kyle-gross <kyg555@outlook.com>
2021-05-27 22:59:04 +0000

    Add ASC order for tv_genres.name

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,4 +3,4 @@ SELECT name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.title = 'Dexter';
+WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name ASC;
