98ae92b06ceed469520ce9b41034a313f8744b3f
kyle-gross <kyg555@outlook.com>
2021-05-27 21:46:02 +0000

    Change count to number_of_shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
 LEFT JOIN tv_genres ON tv_genres.id = genre_id
 GROUP BY genre_id
-ORDER BY COUNT(*) DESC;
+ORDER BY number_of_shows DESC;
