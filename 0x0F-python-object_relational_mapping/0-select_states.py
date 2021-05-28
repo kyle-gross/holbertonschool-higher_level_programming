697b240ee9af9e73082d69dfb673c4e148190494
kyle-gross <kyg555@outlook.com>
2021-05-28 23:06:55 +0000

    add port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Lists all statesfrom the hbtn_0e_0_usa database"""
+"""Lists all states from the hbtn_0e_0_usa database"""
 
 
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
 
-    db_connection = MySQLdb.connect('localhost',
+    db_connection = MySQLdb.connect('localhost:3306',
                                     argv[1],
                                     argv[2],
                                     argv[3])
