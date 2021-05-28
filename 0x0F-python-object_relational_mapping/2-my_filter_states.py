fafb9bbe22da48621098f1c54839d5a7d73a4151
kyle-gross <kyg555@outlook.com>
2021-05-28 23:26:47 +0000

    Add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists all states that match argv[4] from the hbtn_0e_0_usa database"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db_connection = MySQLdb.connect('localhost',
+                                    argv[1],
+                                    argv[2],
+                                    argv[3])
+
+    cursor = db_connection.cursor()
+    cursor.execute('SELECT * FROM states')
+
+    states = cursor.fetchall()
+
+    for i in range(len(states)):
+        if (argv[4] == states[i][1]):
+            print(states[i])
