ae2ac488ea827f25503cfa27a39ad9120303a253
kyle-gross <kyg555@outlook.com>
2021-05-29 20:51:23 +0000

    Reformat to make nice

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """This module contains the class State"""
-
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
 
 Base = declarative_base()
 
