a4a820d56f0f86ad3056bb35d3a61ac0d85cafc0
kyle-gross <kyg555@outlook.com>
2021-05-29 22:04:20 +0000

    Add task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module lists items in the state table"""
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1],
+                                   argv[2],
+                                   argv[3],
+                                   pool_pre_ping=True))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State)
+
+    for state in states:
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
