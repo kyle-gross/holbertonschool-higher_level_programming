91cb7ae6051b5b0cb1d54c62913df7b0989f050d
kyle-gross <kyg555@outlook.com>
2021-05-29 00:02:50 +0000

    Add task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Safely lists all states that match argv[4] from the database"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db_connection = MySQLdb.connect('localhost',
+                                    argv[1],
+                                    argv[2],
+                                    argv[3])
+
+    cursor = db_connection.cursor()
+    with db_connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT 
+                *
+            FROM
+                states
+            WHERE
+                name = BINARY %(name)s
+            """, {
+                'name': argv[4]
+            })
+
+        states = cursor.fetchall()
+
+    for i in range(len(states)):
+        print(states[i])
