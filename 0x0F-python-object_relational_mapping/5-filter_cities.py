9b0146e85f01a4ca45501ebe60c2c0671570351c
kyle-gross <kyg555@outlook.com>
2021-05-29 19:45:01 +0000

    Add task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Safely lists all cities that from the hbtn_0e_0_usa database"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db_connection = MySQLdb.connect('localhost',
+                                    argv[1],
+                                    argv[2],
+                                    argv[3])
+
+    with db_connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT
+                cities.name
+            FROM
+                states
+            JOIN
+                cities
+            ON
+                states.id = cities.state_id
+            WHERE
+                states.name = BINARY %(states)s
+            """, {
+                'states': argv[4]
+            })
+
+        cities = cursor.fetchall()
+
+    for i in range(len(cities)):
+        print(cities[i][0], end="")
+        if i < (len(cities) - 1):
+            print("", end=", ")
+
+    print()
