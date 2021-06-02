5efc5c82f1140394d6cb0611e5947017b30b5a61
kyle-gross <kyg555@outlook.com>
2021-06-02 19:50:20 +0000

    Add task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const myArgs = process.argv.slice(2);
+
+if (typeof myArgs[0] === 'undefined' || isNaN(myArgs[0])) {
+  console.log('%i', 0);
+} else {
+  console.log(secondMax(myArgs));
+}
+
+function secondMax (myArr) {
+  let max = -Infinity;
+  let result = -Infinity;
+
+  for (const value of myArr) {
+    const nr = Number(value);
+
+    if (nr > max) {
+      [result, max] = [max, nr];
+    } else if (nr < max && nr > result) {
+      result = nr;
+    }
+  }
+  return (result);
+}
