e10c92e49ed169e9b07bb9fd6e29bcbf70fe9daa
kyle-gross <kyg555@outlook.com>
2021-06-02 18:43:07 +0000

    Add NaN check

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const myArgs = process.argv.slice(2);
 
-if (typeof myArgs[0] === 'undefined') {
+if (typeof myArgs[0] === 'undefined' || isNaN(myArgs[0])) {
   console.log('Not a number');
 } else {
   console.log('My number: %i', myArgs[0]);
