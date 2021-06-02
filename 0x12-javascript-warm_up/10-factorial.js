17fac105575b5c429d3c6e148d44ee5c35419303
kyle-gross <kyg555@outlook.com>
2021-06-02 19:30:52 +0000

    Add check for no args

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,8 +2,12 @@
 
 const myArgs = process.argv.slice(2);
 
-console.log(factorial(myArgs[0]));
+if (typeof myArgs[0] === 'undefined' || isNaN(myArgs[0])) {
+  console.log('%i', 1);
+} else {
+  console.log(factorial(myArgs[0]));
+}
 
 function factorial (n) {
-  return (n != 1) ? n * factorial(n - 1) : 1;
+  return (n !== 1) ? n * factorial(n - 1) : 1;
 }
